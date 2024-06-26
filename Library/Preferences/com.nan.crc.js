ace7aa2224579751f9f8c90f59ebfc6b2dd4ac58
zhaonan <35675770+invalidunit@users.noreply.github.com>
2024-06-26 13:45:27 +0800

    Create com.nan.crc.js

---
+++ Library/Preferences/com.nan.crc.js
@@ -0,0 +1,40 @@
+{
+  "source": [
+    {
+      "enable": 1,
+      "url": "https://invalidunit.github.io/repo/"
+    },
+    {
+      "enable": 1,
+      "url": "https://invalidunit.github.io/repo2/"
+    },
+    {
+      "enable": 1,
+      "url": "https://ellekit.space/"
+    },
+    {
+      "enable": 1,
+      "url": "https://repo.chariz.com/"
+    },
+    {
+      "enable": 1,
+      "url": "https://havoc.app/"
+    }
+  ],
+  "enable": 1,
+  "filter_architecture": "all",
+  "use_name": 1,
+  "bark": {
+    "is_archive": 0,
+    "Hello_World": 0,
+    "level": "active",
+    "title": "来自 crc - repo_name 已变更",
+    "group": "com.nan.crc",
+    "body": {
+      "add": "下列软件包已新增：",
+      "del": "下列软件包已移除：",
+      "up": "下列软件包已更新："
+    },
+    "sound": "healthnotification"
+  }
+}
