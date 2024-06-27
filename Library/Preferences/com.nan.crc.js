24aadba18004fd428149ee5f1ad5182eef430197
zhaonan <35675770+invalidunit@users.noreply.github.com>
2024-06-27 08:25:18 +0800

    Update com.nan.crc.js

--- Library/Preferences/com.nan.crc.js
+++ Library/Preferences/com.nan.crc.js
@@ -1,4 +1,20 @@
 {
+  "bark": {
+    "is_archive": 0,
+    "Hello_World": 0,
+    "level": "active",
+    "title": "来自 crc - repo_name 已变更",
+    "group": "com.nan.crc",
+    "body": {
+      "add": "下列软件包已新增：",
+      "del": "下列软件包已移除：",
+      "up": "下列软件包已更新："
+    },
+    "sound": "healthnotification"
+  },
+  "enable": 1,
+  "use_name": 0,
+  "filter_architecture": "all",
   "source": [
     {
       "enable": 1,
@@ -19,22 +35,14 @@
     {
       "enable": 1,
       "url": "https://havoc.app/"
-    }
-  ],
-  "enable": 1,
-  "filter_architecture": "all",
-  "use_name": 1,
-  "bark": {
-    "is_archive": 0,
-    "Hello_World": 0,
-    "level": "active",
-    "title": "来自 crc - repo_name 已变更",
-    "group": "com.nan.crc",
-    "body": {
-      "add": "下列软件包已新增：",
-      "del": "下列软件包已移除：",
-      "up": "下列软件包已更新："
     },
-    "sound": "healthnotification"
-  }
+    {
+      "enable": 1,
+      "url": "https://flyx099.github.io/repo/"
+    },
+    {
+      "enable": 1,
+      "url": "https://jailrepo.initnil.com/"
+    }
+  ]
 }
